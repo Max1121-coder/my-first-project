1f0c67f3d2fd74f0d569d3e63745b54ef638bb9e
Max Muller <maxmuller001121@gmail.com>
2025-05-10 16:21:42 +0200

    app.js is completed.

---
+++ Blog/server/app.js
@@ -0,0 +1,30 @@
+// app.js - Express app setup
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/api', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+module.exports = app;
