56ea9f4ab225290433856ed6a3235314b5429d9a
Max Muller <maxmuller001121@gmail.com>
2025-05-10 16:32:21 +0200

    controllers/authController.js is changed

--- Blog/server/controllers/authController.js
+++ Blog/server/controllers/authController.js
@@ -13,3 +13,12 @@ exports.register = async (req, res) => {
     res.status(400).send('Error creating user');
   }
 };
+
+exports.login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+    return res.status(401).send('Invalid credentials');
+  }
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  res.send({ token });
+};
